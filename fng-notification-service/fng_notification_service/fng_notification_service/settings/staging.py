afa3a23b05faaedf8ec66066457e57917f017cfa
BHAVIKA BADJATE <64466317+bhavikabadjate@users.noreply.github.com>
2021-02-09 18:47:57 +0530

    notifiaction service

---
+++ fng-notification-service/fng_notification_service/fng_notification_service/settings/staging.py
@@ -0,0 +1,19 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'FNG-UserService',
+       'USER': 'postgres',
+       'PASSWORD': 'Bhavika@123',
+    }
+}
+
+
+# EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'wefreaksngeeks@gmail.com'
+EMAIL_HOST_PASSWORD = 'hbnlftpoiwzsdaft'
